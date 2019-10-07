cc1cd7e99d50375b78fe37ddad6ccb83b8785d1b
unknown <abayiringira1994@yahoo.com>
2019-10-07 01:24:42 -0700

    adding step 18 final

---
+++ task-of-everyLeaf-co-nextStep/README.md
@@ -0,0 +1,27 @@
+USER
+
+column_name | data type
+------------|----------
+user_id     |integer
+F_Name      |string
+L_Name      |string
+Email       |string
+profile     |text
+
+TASK
+
+column_name | data type
+------------|----------
+task_id     |integer
+task_tittle |string
+task_number |string
+comment     |string
+
+LABLE
+
+column_name | data type
+------------|----------
+lable_id    |integer
+lable_name  |string
+
+
---
+++ task-of-everyLeaf-co-nextStep/spec/features/task.spec.rb
@@ -0,0 +1,26 @@
+
+require 'rails_helper'
+RSpec.feature "Task management function", type: :feature do
+    background do
+        Task.create!(name: 'adax', details: 'time')
+        Task.create!(name: 'adali', details: 'igihe')
+      end
+  scenario "Test task list" do
+  visit tasks_path
+  expect(page).to have_content 'time'
+  expect(page).to have_content 'igihe'
+  end
+  scenario "Test task creation" do
+  visit new_task_path 
+  fill_in 'Name', with: 'janvier'
+  fill_in 'Details', with: 'monday'
+ click_button '登録する'
+ visit tasks_path
+ expect(page).to have_content 'janvier'
+ expect(page).to have_content 'monday'
+  end
+  scenario "Test task details" do
+  end
+  scenario "Test whether tasks are arranged in descending order of creation date" do
+  end
+end
